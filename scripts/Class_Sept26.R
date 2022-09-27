abe23ac2b99b9704e74499e111f3f6c9cff5f61c
Basanta Khakurel <basanta.khakurel@selu.edu>
2022-09-27 14:57:12 -0500

    Added a function to drop 'NA' values and create a boxplot

--- scripts/Class_Sept26.R
+++ scripts/Class_Sept26.R
@@ -7,3 +7,14 @@ filter_years <- function(dataset = data){
   filter(years>1997)
 return(new_data)
 }
+
+drop_na_butterflies <- function(dataset = data){
+  no_na_data <- data %>% 
+    drop_na()
+  return(no_na_data)
+}
+
+boxplot_butterflies <- function(dataset = data){
+  spring_plot <- ggplot(data, mapping = aes(ButterflySpecies, SpringTemp))+geom_boxplot()
+  return(spring_plot)
+}
